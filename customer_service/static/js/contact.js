24dde4f04dc604048cb2b42472bdf13080e65e04
wangxb <wangxb@builds-iMac-3.local>
2017-11-14 13:38:51 +0800

    add notes checked, if no status selected, notes can not be saved

--- customer_service/static/js/contact.js
+++ customer_service/static/js/contact.js
@@ -3,49 +3,53 @@
  */
 
 function save(url, callback) {
-    var top_alert_div = $("div.top-alert");
-    var top_content = $("#top-alert-content");
-    var tmp_alert_class = "alert top-alert alert-dismissible show col-md-6 col-lg-offset-3";
-    var top_alert_class;
-    if (is_form_modified()) {
-        var form_data = $("form").serialize();
-        $.post(url, form_data, function(data, status) {
-            if (status == "success") {
-                if (data.code == 0) {
-                    top_alert_class = tmp_alert_class + ' alert-success';
-                    if ($("input[name=form_changed]").length > 0)
-                        $("input[name=form_changed]").attr({value: 0});
-                    window.origin_form_data = $("form").serializeArray();
-                } else {
-                    top_alert_class = tmp_alert_class + ' alert-danger';
-                }
-                top_alert_div.attr({'class': top_alert_class});
-                top_content.html(data.message);
+    $("form").isValid( function(v) {
+        if (v) {
+            var top_alert_div = $("div.top-alert");
+            var top_content = $("#top-alert-content");
+            var tmp_alert_class = "alert top-alert alert-dismissible show col-md-6 col-lg-offset-3";
+            var top_alert_class;
+            if (is_form_modified()) {
+                var form_data = $("form").serialize();
+                $.post(url, form_data, function (data, status) {
+                    if (status == "success") {
+                        if (data.code == 0) {
+                            top_alert_class = tmp_alert_class + ' alert-success';
+                            if ($("input[name=form_changed]").length > 0)
+                                $("input[name=form_changed]").attr({value: 0});
+                            window.origin_form_data = $("form").serializeArray();
+                        } else {
+                            top_alert_class = tmp_alert_class + ' alert-danger';
+                        }
+                        top_alert_div.attr({'class': top_alert_class});
+                        top_content.html(data.message);
+                    } else {
+                        top_alert_class += ' alert-danger';
+                        top_alert_div.attr({'class': top_alert_class});
+                        top_content.html('服务器内部错误！');
+                    }
+
+                    top_alert_div.css({top: '-' + (top_alert_div.height() + 24) + 'px'});
+
+                    if (status == 'success' && data.code == 0 && callback != undefined) {
+                        $("div.top-alert").animate({top: '0'}, 'slow', callback);
+                    } else
+                        $("div.top-alert").animate({top: '0'}, 'slow');
+
+                }, 'json');
             } else {
-                top_alert_class += ' alert-danger';
+                top_alert_class = tmp_alert_class + ' alert-success';
                 top_alert_div.attr({'class': top_alert_class});
-                top_content.html('服务器内部错误！');
-            }
-
-            top_alert_div.css({top: '-' + (top_alert_div.height() + 24) + 'px'});
-
-            if ( status == 'success' && data.code == 0 && callback != undefined) {
-                $("div.top-alert").animate({top: '0'}, 'slow', callback);
-            } else
+                top_content.html('保存成功 ！');
+                top_alert_div.css({top: '-' + (top_alert_div.height() + 24) + 'px'});
                 $("div.top-alert").animate({top: '0'}, 'slow');
 
-        }, 'json');
-    } else {
-        top_alert_class = tmp_alert_class + ' alert-success';
-        top_alert_div.attr({'class': top_alert_class});
-        top_content.html('保存成功 ！');
-        top_alert_div.css({top: '-' + (top_alert_div.height() + 24) + 'px'});
-        $("div.top-alert").animate({top: '0'}, 'slow');
-
-        if (callback != undefined) {
-            callback();
+                if (callback != undefined) {
+                    callback();
+                }
+            }
         }
-    }
+    })
 }
 
 function save_and_next(url, next_url) {
